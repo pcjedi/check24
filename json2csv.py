edfbf5cb4478e1521475edbcb6e35823b22cb83e
Georg Wendorf <georgwendorf@gmail.com>
2021-03-07 14:05:05 +0100

    Update json2csv.py

--- json2csv.py
+++ json2csv.py
@@ -6,3 +6,28 @@ with open('rows.json') as f:
 
 df = df[df.columns[~df.applymap(lambda x: isinstance(x, list)).any()]]
 df.to_csv("rows.csv", index=False)
+
+for c in df.columns:
+  print(c)
+
+try:
+  df[
+      [
+          "pricelayer.prices.effective.amount",
+          "pricelayer.provider.name",
+          "tariff.names.default",
+          "tariff.network.name",
+          "tariff.internet.pricePerUnit.price.amount",
+          "tariff.internet.traffic.value",
+          "tariff.internet.speed.value",
+          "tariff.ageCheck",
+          "tariff.voice.highestPrice.amount",
+          "tariff.voice.flatrate",
+          "tariff.sms.highestPrice.amount",
+          "tariff.sms.flatrate",
+          "tariff.contract.periods.contract.value",
+          "tariff.contract.periods.contract.unit",
+      ]
+  ].to_csv("rows-simple.csv", index=False)
+except KeyError as e:
+  print(e)
