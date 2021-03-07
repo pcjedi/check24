bef14a334bc5a0c7cf653e9b850e2432ceebd378
Georg Wendorf <georgwendorf@gmail.com>
2021-03-07 12:32:40 +0100

    Update json2csv.py

--- json2csv.py
+++ json2csv.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import json
 
+with open('rows.json') as f:
+  df = pd.json_normalize(json.load(f)['rows']).dropna(axis=1, how='all')
+
+df = df[df.columns[~df.applymap(lambda x: isinstance(x, list)).any()]]
+df.to_csv("rows.csv", index=False)
