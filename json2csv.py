bb5cc8bcf26eb952a8e3c029a5993f913d2d936e
Georg Wendorf <georgwendorf@gmail.com>
2021-03-08 20:17:55 +0000

    Update json2csv.py

--- json2csv.py
+++ json2csv.py
@@ -12,27 +12,25 @@ with open('rows.json') as f:
   df = pd.json_normalize(json.load(f)['rows']).dropna(axis=1, how='all')
 
 df = df[df.columns[~df.applymap(lambda x: isinstance(x, list)).any()]]
-df_simple = df[
-  [
-    "pricelayer.prices.effective.amount",
-    "pricelayer.provider.name",
-    "tariff.names.default",
-    "tariff.network.name",
-    "tariff.internet.pricePerUnit.price.amount",
-    "tariff.internet.traffic.value",
-    "tariff.internet.speed.value",
-    "tariff.ageCheck",
-    "tariff.voice.highestPrice.amount",
-    "tariff.voice.flatrate",
-    "tariff.sms.highestPrice.amount",
-    "tariff.sms.flatrate",
-    "tariff.contract.periods.contract.value",
-    "tariff.contract.periods.contract.unit",
-  ]
+core = [
+  "pricelayer.prices.effective.amount",
+  "pricelayer.provider.name",
+  "tariff.names.default",
+  "tariff.network.name",
+  "tariff.internet.pricePerUnit.price.amount",
+  "tariff.internet.traffic.value",
+  "tariff.internet.speed.value",
+  "tariff.ageCheck",
+  "tariff.voice.highestPrice.amount",
+  "tariff.voice.flatrate",
+  "tariff.sms.highestPrice.amount",
+  "tariff.sms.flatrate",
+  "tariff.contract.periods.contract.value",
+  "tariff.contract.periods.contract.unit",
 ]
 
 if args.check24core:
-  df_simple.to_csv("rows-simple.csv", index=False)
+  df[core].to_csv("rows-simple.csv", index=False)
 
 if args.check24all:
   df.to_csv("rows.csv", index=False)
@@ -40,7 +38,7 @@ if args.check24all:
     print(c)
 
 if args.check24adjusted:
-  df_simple.replace(
+  df[core].replace(
     to_replace = {
       'pricelayer.provider.name': {"web.de": "gmx.de"}
     }
